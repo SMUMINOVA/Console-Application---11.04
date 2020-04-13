bf263713ef02a75ff2a3b1af8a0019ddd452e96b
Sobira <sobira204@gmail.com>
2020-04-13 21:00:52 +0500

    using class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace HW11._04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("How many cas do you have in your salon?");
+            int numb = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("What kind of car do you have in your salon?");
+            Avto [] cars = new Avto [numb];
+            for (int i = 0; i < numb; i++){
+                System.Console.Write("Enter name of the car: ");
+                cars[i].nameOfModel = Console.ReadLine();
+                System.Console.Write("Enter year of issue: ");
+                cars[i].yearOfIssue = int.Parse(Console.ReadLine());
+                System.Console.Write("Enter max speed of car: ");
+                cars[i].maxSpeed = int.Parse(Console.ReadLine());
+                System.Console.Write("Enter price of car: ");
+                cars[i].priceOfCar = int.Parse(Console.ReadLine());
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine("Your collection:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach(var m in cars)
+            m.showInfo();
+            Console.ReadKey();
         }
         class Avto{
             public string nameOfModel{get; set;}
